3de8317805fda4475b4f25bb092b16125b8a1eb3
stravis11 <stevetravis11@gmail.com>
2019-05-24 09:35:04 -0400

    WIP: Updated survey questions.

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
-require("dotenv").config();
-
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-
-// const {
-//   env: { DB_HOST, DB_USER, DB_PASSWORD }
-// } = process;
-
-// // create the connection information for the sql database
-// const db = mysql.createConnection({
-//   // hostname, stored in .env
-//   host: DB_HOST,
-
-//   // Your port; if not 3306
-//   port: 3306,
-
-//   // Your username, stored in .env
-//   user: DB_USER,
-
-//   // Your password, stored in .env
-//   password: DB_PASSWORD,
-//   database: "friendfinder"
-// });
 
 const app = express();
 
@@ -37,8 +15,4 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// db.connect(err => {
-//   if (err) throw err;
-
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-// });
