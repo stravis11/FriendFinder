9b65be9f7b8499a340f27b500d03945bc448c211
stravis11 <stevetravis11@gmail.com>
2019-05-26 11:57:22 -0400

    Changed form validation for survey.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,37 +1,13 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 const friendsData = require("../data/friends");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
   // API POST Requests
-  // Below code handles when a user submits a survey and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a survey... this data is then sent to the server...
-  // Then the server saves the data to the friendsData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // console.log(friendsData[0].scores);
-    // console.log(req.body.scores);
     if (friendsData.length > 0) {
       var indexOfClosestMatch = 0;
       var lowestDif;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,7 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 const path = require("path");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
+// Routing to survey
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
